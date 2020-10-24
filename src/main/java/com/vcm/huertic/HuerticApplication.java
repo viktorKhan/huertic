526d2370e3a99ba141f160def1fa24d8dca784d8
viktorKhan <victor.canizares.mata@gmail.com>
2020-10-24 22:06:16 +0200

    Initial SpringBoot configuration

---
+++ src/main/java/com/vcm/huertic/HuerticApplication.java
@@ -0,0 +1,13 @@
+package com.vcm.huertic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuerticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuerticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcm/huertic/HuerticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcm.huertic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuerticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
